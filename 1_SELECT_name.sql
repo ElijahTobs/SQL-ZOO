1b9106a7ac4b356e5fde0563b24ab94ff816710c
Elijah <eligenius@gmail.com>
2020-07-27 20:59:05 +0100

    Add more examples for select_name

--- 1_SELECT_name.sql
+++ 1_SELECT_name.sql
@@ -23,3 +23,18 @@ SELECT name FROM world
   WHERE name LIKE '%oo%'
 
 -- 7. Find the countries that have three or more a in the name
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+-- 8. Find the countries that have "t" as the second character.
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+
+-- 9. Find the countries that have two "o" characters separated by two others.
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+
+--  10. Find the countries that have exactly four characters.
+SELECT name FROM world
+ WHERE name LIKE '____'
